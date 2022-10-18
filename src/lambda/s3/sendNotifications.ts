e14fb982c915f9bf21e840f1903b77109f94f8db
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-10-18 17:10:40 +0300

    (feat) connect sns to s3 bucket

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import {S3Handler, S3Event} from 'aws-lambda';
+import {SNSHandler, SNSEvent, S3Event} from 'aws-lambda';
 import 'source-map-support/register';
 import * as AWS from 'aws-sdk';
 
@@ -15,9 +15,19 @@ const connectionParams = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams);
 
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  console.log('Processing SNS event ', JSON.stringify(event))
+  for (const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message
+    console.log('Processing S3 event', s3EventStr)
+    const s3Event = JSON.parse(s3EventStr)
 
-export const handler: S3Handler = async (event: S3Event) => {
-  for (const record of event.Records) {
+    await processS3Event(s3Event)
+  }
+}
+
+async function processS3Event(s3event: S3Event) {
+  for (const record of s3event.Records) {
     const key  = record.s3.object.key;
     console.log('Processing S3 item with key: ', key);
 
