5944695c0d36fb332b36c0812dda5da7615cd181
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-10-27 13:31:47 +0300

    (feat) add jsonwebtoken for auth

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,13 +1,25 @@
 import {CustomAuthorizerResult, APIGatewayTokenAuthorizerEvent} from 'aws-lambda';
 import 'source-map-support/register';
 
+import { JwtToken } from 'src/auth/JwtToken';
+import * as AWS from 'aws-sdk';
+import {verify} from 'jsonwebtoken';
+
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+// Cache client if a Lambda instance is reused
+let cachedSecret: string
+
 export const handler = async (event: APIGatewayTokenAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
-    verifyToken(event.authorizationToken);
+    const decodedToken = await verifyToken(event.authorizationToken);
     console.log('User was authorized');
 
     return {
-      principalId: 'user',
+      principalId: decodedToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -37,7 +49,7 @@ export const handler = async (event: APIGatewayTokenAuthorizerEvent): Promise<Cu
     }
   }
 
-  function verifyToken(authHeader: string) {
+  async function verifyToken(authHeader: string): Promise<JwtToken> {
     if(!authHeader)
       throw new Error('No authorization header');
 
@@ -47,10 +59,25 @@ export const handler = async (event: APIGatewayTokenAuthorizerEvent): Promise<Cu
     const split = authHeader.split(' ');
     const token = split[1];
 
-    if (token !== '123')
-      throw new Error('Invalid token');
+    const secretObject: any = await getSecret();
+    const secret = secretObject[secretField];
+
+    return verify(token, secret) as JwtToken;
 
     // Request has been authorized
+  }
+
+  async function getSecret() {
+    if (cachedSecret) return cachedSecret;
+
+    const data = await client.getSecretValue({
+      SecretId: secretId
+    }) 
+    .promise();
+
+    cachedSecret = data.SecretString;
+
+    return JSON.parse(cachedSecret);
   }
 
 }
